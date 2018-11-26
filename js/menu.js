2a418f910c03db8483c3aeeed23dc2596c535847
haolushan5253 <2587051793@qq.com>
2018-11-26 20:19:41 +0800

    网站运用jquery

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,16 @@ var oMenu = document.getElementById('menu');
 var oClose = document.getElementById('close');
 var oSide = document.getElementById('side');
 var oHeader = document.getElementById('header');
+// var oLunbo = document.getElementById('lunbo');
+// var odemo = document.getElementById('demo');
+// window.onload = function(){
+//     var lHeight = $(window).height();
+//     oLunbo.style.height = lHeight + 'px';
+//     odemo.style.height = lHeight + 'px';
+//     console.log(lHeight);
+//     console.log(oLunbo.style.height);
+//     console.log(odemo.style.height);
+// };
 oMenu.addEventListener('click',function(){
     oMenu.style.display = "none";
     oSide.style.transition = 'all 1s linear';
--- js/pulley.js
+++ js/pulley.js
@@ -1,3 +1,50 @@
 $(function(){
-    
-})
+    // $(window).scroll(function(){
+        setInterval(function(){
+            var winPos = $(document).scrollTop();
+            var winHeight =  $(window).height();  
+            var oLunbo = document.getElementById('lunbo'); 
+            var olunboH = oLunbo.style.height;
+            console.log(olunboH);
+            if(winPos<winHeight && winPos>winHeight/2){
+            window.scrollTo(0,winHeight);
+            // console.log(winPos,winHeight);
+            }else if(winPos>winHeight*3/2 && winPos<2*winHeight){
+                window.scrollTo(0,2*winHeight);
+                // console.log(winPos,winHeight);
+            }else if(winPos>winHeight*5/2 && winPos<(2*winHeight + olunboH)){
+                window.scrollTo(0,(2*winHeight + olunboH));
+                // console.log(winPos,winHeight);
+            }else if(winPos>(winHeight*5/2 + olunboH) && winPos<(3*winHeight + olunboH)){
+                window.scrollTo(0,(3*winHeight + olunboH));
+                // console.log(winPos,winHeight);
+            }else if(winPos>(winHeight*7/2 + olunboH) && winPos<(4*winHeight + olunboH)){
+                window.scrollTo(0,(4*winHeight + olunboH));
+                // console.log(winPos,winHeight);
+            }else if(winPos>(winHeight*9/2 + olunboH) && winPos<(5*winHeight + olunboH)){
+                window.scrollTo(0,(5*winHeight + olunboH));
+                // console.log(winPos,winHeight);
+            }
+
+            else if(winPos<winHeight/2){
+                window.scrollTo(0,0);
+                // console.log(winPos,winHeight);
+            }
+            else if(winPos<winHeight*3/2 && winPos>winHeight){
+                window.scrollTo(0,winHeight);
+                // console.log(winPos,winHeight);
+            }else if(winPos<winHeight*5/2 && winPos>2*winHeight){
+                window.scrollTo(0,(2*winHeight));
+                // console.log(winPos,winHeight);
+            }else if(winPos<(winHeight*5/2 + olunboH) && winPos>(2*winHeight + olunboH)){
+                window.scrollTo(0,(2*winHeight + olunboH));
+                // console.log(winPos,winHeight);
+            }else if(winPos<(winHeight*7/2 + olunboH) && winPos>(3*winHeight + olunboH)){
+                window.scrollTo(0,(3*winHeight + olunboH));
+                // console.log(winPos,winHeight);
+            }else if(winPos<(winHeight*9/2 + olunboH) && winPos>(4*winHeight + oLunbo)){
+                window.scrollTo(0,(4*winHeight + olunboH));
+            }
+     },1000)
+});
+// });
